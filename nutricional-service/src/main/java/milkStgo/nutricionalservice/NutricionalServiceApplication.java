9b57ba853c06813e761fe7d5037cf35826c94278
Diego Oliva Lopez <diego.oliva@usach.cl>
2023-06-13 01:52:38 -0400

    Microservicios esenciales

---
+++ nutricional-service/src/main/java/milkStgo/nutricionalservice/NutricionalServiceApplication.java
@@ -0,0 +1,13 @@
+package milkStgo.nutricionalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricionalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricionalServiceApplication.class, args);
+	}
+
+}
---
+++ nutricional-service/src/test/java/milkStgo/nutricionalservice/NutricionalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package milkStgo.nutricionalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricionalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
